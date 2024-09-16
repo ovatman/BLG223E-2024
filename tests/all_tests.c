b2e03e6b3fc3701a9407d84125f6fe3f06afe618
Tolga Ovatman <tolga.ovatman@icrontech.com>
2024-09-16 15:36:30 +0300

    emptied main branch

--- tests/all_tests.c
+++
@@ -1,6 +0,0 @@
-#include "munit.h"
-#include "expanding_array_tests.h"
-
-int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
-    return munit_suite_main(&test_suite, NULL, argc, argv);
-}

dfd138693922e1c07018078a154ddfdc07c08544
Tolga Ovatman <ovatman@gmail.com>
2024-11-27 12:25:39 +0000

    updated main to match the latest content

--- src/main.c
+++ src/main.c
@@ -5,8 +5,26 @@ using namespace std;
 
 int main(){
     TREE t; 
-    populate_tree(&t);
+    init_tree(t);
 
-    cout << t.root->left->right->left->data << endl;
+    add_element(t, 10);
+    add_element(t, 5);
+    add_element(t, 15);
+    add_element(t, 3);
+    add_element(t, 12);
+    add_element(t, 9);
+    add_element(t, 1);
+    add_element(t, 4);
+
+    remove_element(t, 5);
+
+    preorder_traverse_tree(t);
+    inorder_traverse_tree(t);
+    postorder_traverse_tree(t);
+    breadth_first_traverse(t);
+
+    search_bin_tree(t, 65);
+
+    destruct_tree(t);
     return 0;
 }
